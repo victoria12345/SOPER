a6107476dfd410152b4bb339b058814a5c2d4dea
Nachocho11 <theigragar11@gmail.com>
2018-03-16 17:02:10 +0100

    Add files via upload

---
+++ Practica2/ejercicio2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
+
+int main(int argc, char const *argv[]) {
+  int i;
+  int pid;
+
+  for (i = 0; i < 4; i++) {
+    pid = fork();
+    if(pid == 0){
+      fprintf(stdout, "Soy el proceso hijo %d\n", getpid());
+      sleep(30);
+      fprintf(stdout, "Soy el proceso hijo %d y ya me toca terminar", getpid());
+      exit(EXIT_SUCCESS);
+    }else{
+      sleep(5);
+      kill(pid, SIGTERM);
+    }
+  }
+  return 0;
+}
